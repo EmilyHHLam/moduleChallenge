911688b502bb47222ba16cde1ec1822a387525a8
EmilyH <EmilyHHLam@gmail.com>
2017-03-15 00:30:41 -0500

    add the comments to instruct what the code does

--- modules/USConversion.js
+++ modules/USConversion.js
@@ -1,24 +1,15 @@
 //get the ranNumb and turn into USA string value
 // var http = require('http');
-// var cat = require('M1.js');
+//IMPORT
+//var convertUSD = require('./USConversion.js');
 
 function convertFunc(ranNumb) {
   //var value = ranNumb;
-
+ //get the number and turn it the the dollar format and return it as a string
   var balance = '$' + ranNumb.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
   //console.log(num);
   return balance;
  }
 
 
-
-  //return someUSD;
-
-
-
-// console.log(ranNumb);
-
-//var scottsVar = 10;
-
-//Scott Code
 module.exports = convertFunc;
